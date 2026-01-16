39023c2964458c99c7b7deba35cbd6f0dea1a29b
User <user@MacBook-Pro.local>
2026-01-16 18:36:30 +0100

    fix: hardcode database url injection in prisma service

--- apps/api-core/src/prisma/prisma.service.ts
+++ apps/api-core/src/prisma/prisma.service.ts
@@ -1,8 +1,27 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from 'database';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor() {
+        // 🚨 FIX: On force l'URL explicitement ici
+        const url = process.env.DATABASE_URL;
+
+        if (!url) {
+            console.error('❌ ERREUR CRITIQUE: La variable DATABASE_URL est absente !');
+        } else {
+            console.log('✅ DATABASE_URL trouvée (Lancement de Prisma)');
+        }
+
+        super({
+            datasources: {
+                db: {
+                    url: url,
+                },
+            },
+        });
+    }
+
     async onModuleInit() {
         await this.$connect();
     }
